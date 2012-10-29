e72ccf73465e39308dc14c62e46f2e99818151ef
module17 <module17@gmail.com>
2012-10-29 02:13:44 -0400

    Added logic to process request

--- application/controllers/weather.php
+++ application/controllers/weather.php
@@ -2,14 +2,35 @@
 
 class Weather extends CI_Controller {
 
-	public function index()
+	public function index($url_params=null)
 	{        
         $this->load->model('locations');
-        
-        $data['main_content'] = 'weather';
-        $data['title'] = 'Current Weather';
-        $data['data']['countries'] = $this->locations->get_all_countries();
+        $this->load->model('geocode');
 
+        $data['main_content'] = 'weather';
+        $data['title'] = 'currentweather';
+        $data['data']['countries'] = $this->locations->get_all_countries();
+        if ($url_params) {
+            $this->locations->lookup_request($url_params);
+        }
+        if ($this->locations->request_type == 'IP') {
+            $this->geocode->lookup_ip();
+        }
+
+        //if country, show regions
+        if ($this->locations->request_type == 'COUNTRY') {
+
+            //$data['data']['regions'] = $this->locations->get_regions();
+        }
+        //if region, show cities
+        if ($this->locations->request_type == 'REGION') {
+            //$data['data']['cities'] = $this->locations->get_cities();
+        }
+
+        //set locations data
+        $data['data']['location_data'] = $this->locations->location_data;
+
+        $data['data']['request_type'] = $this->locations->request_type;
         $this->load->view('template', $data);
 
 	}
