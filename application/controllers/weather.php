fdbf31844d485cc7763b719fb6d17872366e82de
module17 <module17@gmail.com>
2012-10-30 12:22:23 -0400

    Add weather and youtube data logic

--- application/controllers/weather.php
+++ application/controllers/weather.php
@@ -6,6 +6,7 @@ class Weather extends CI_Controller {
 	{        
         $this->load->model('locations');
         $this->load->model('geocode');
+        $this->load->model('weather_data');
 
         $data['main_content'] = 'weather';
         $data['title'] = 'currentweather';
@@ -13,6 +14,7 @@ class Weather extends CI_Controller {
         if ($url_params) {
             $this->locations->lookup_request($url_params);
         }
+        //if there is no request in the url proceed to determine ip based location
         if ($this->locations->request_type == 'IP') {
             $this->geocode->lookup_ip();
         }
@@ -27,10 +29,27 @@ class Weather extends CI_Controller {
             //$data['data']['cities'] = $this->locations->get_cities();
         }
 
+        //lookup the weather
+        $data['data']['weather_data'] = $this->weather_data->lookup_weather();
+
+        //get videos
+        $data['data']['youtube_data'] = $this->youtube->get_videos($this->locations->location_data['latitude'],
+                                                                    $this->locations->location_data['longitude']);
+
+        $map_config['center'] = $this->locations->location_data['latitude'] . ', ' . $this->locations->location_data['longitude'];
+        $map_config['map_height'] = '170px';
+        //initialize our map passing config parameters
+        $this->googlemaps->initialize($map_config);
+        //create the map and send to view
+        $data['data']['map'] = $this->googlemaps->create_map();
+
         //set locations data
         $data['data']['location_data'] = $this->locations->location_data;
 
+        //set the request type variable to the view
         $data['data']['request_type'] = $this->locations->request_type;
+
+        //load main template view
         $this->load->view('template', $data);
 
 	}
