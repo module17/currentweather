09468c8c172ce531a563e6a5b6866e468ab44b80
module17 <module17@gmail.com>
2012-10-30 12:26:17 -0400

    Add weather and youtube display

--- application/views/weather.php
+++ application/views/weather.php
@@ -22,18 +22,36 @@
         </ul>
         <?php endif; ?>
 
+        <?php echo $map['html']; ?>
+
         <?=$request_type;?>
-                
-        <p><a class="btn" href="#">View details &raquo;</a></p>
+
     </div>
     <div class="span4">
-        <h2>Twitter</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn" href="#">View details &raquo;</a></p>
+        <h2>Weather forecast</h2>
+
+        <?php if ($weather_data): ?>
+        <img src="<?=$weather_data['current_observation']['icon_url']; ?>" />
+        <ul>
+            <li>Current conditions: <?=$weather_data['current_observation']['weather']; ?></li>
+            <li>Temperature: <?=$weather_data['current_observation']['temperature_string']; ?></li>
+            <li>Relative Humidity: <?=$weather_data['current_observation']['relative_humidity']; ?></li>
+            <li>Wind: <?=$weather_data['current_observation']['wind_string']; ?></li>
+        </ul>
+        <?php endif; ?>
+        <p><a class="btn" href="<?=$weather_data['current_observation']['forecast_url']; ?>">View 10 day forecast &raquo;</a></p>
     </div>
     <div class="span4">
         <h2>Youtube</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <?php //var_dump($youtube_data['feed']['entry']); ?>
+        <?php if ($youtube_data): ?>
+        <ul>
+        <?php foreach ($youtube_data['feed']['entry'] as $entry): ?>
+          <li><?=$entry['title']['$t'];?></li>
+        <?php endforeach; ?>
+        </ul>
+        <?php endif; ?>
+
         <p><a class="btn" href="#">View details &raquo;</a></p>
     </div>
     <div class="span4">
