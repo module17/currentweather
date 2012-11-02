0da8071b60fd7524f8636e7b36620cc53ed8f192
module17 <module17@gmail.com>
2012-11-02 02:43:22 -0400

    Add Twitter feed to view

--- application/views/weather.php
+++ application/views/weather.php
@@ -31,7 +31,7 @@
         <h2>Weather forecast</h2>
 
         <?php if ($weather_data): ?>
-        <img src="<?=$weather_data['current_observation']['icon_url']; ?>" />
+        <img class="img-rounded" src="<?=$weather_data['current_observation']['icon_url']; ?>" />
         <ul>
             <li>Current conditions: <?=$weather_data['current_observation']['weather']; ?></li>
             <li>Temperature: <?=$weather_data['current_observation']['temperature_string']; ?></li>
@@ -41,22 +41,34 @@
         <?php endif; ?>
         <p><a class="btn" href="<?=$weather_data['current_observation']['forecast_url']; ?>">View 10 day forecast &raquo;</a></p>
     </div>
-    <div class="span4">
+    <div class="span8">
         <h2>Youtube</h2>
         <?php //var_dump($youtube_data['feed']['entry']); ?>
         <?php if ($youtube_data): ?>
         <ul>
         <?php foreach ($youtube_data['feed']['entry'] as $entry): ?>
-          <li><?=$entry['title']['$t'];?></li>
+          <li><strong><?=$entry['media$group']['media$credit'][0]['$t'];?></strong>
+              <img class="img-rounded" src="<?=$entry['media$group']['media$thumbnail'][0]['url'];?>" />
+              <?=$entry['title']['$t'];?>
+          </li>
         <?php endforeach; ?>
         </ul>
         <?php endif; ?>
-
-        <p><a class="btn" href="#">View details &raquo;</a></p>
     </div>
-    <div class="span4">
-        <h2>Flickr</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+    <div class="span8">
+        <h2>Twitter</h2>
+        <?php if ($twitter_data): ?>
+        <ul>
+            <?php foreach ($twitter_data['results'] as $tweet): ?>
+            <li><strong><?=$tweet['from_user'];?></strong> <?=$tweet['text'];?>
+                <img class="img-rounded" src="<?=$tweet['profile_image_url'];?>" />
+                <?=(isset($tweet['location']))?$tweet['location']:'NULL';?>
+                <span class="then" data-date="<?=$tweet['created_at'];?>"></span>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php endif; ?>
+
         <p><a class="btn" href="#">View details &raquo;</a></p>
     </div>
 </div>
